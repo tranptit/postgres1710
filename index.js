dff3bb437fea92ade02fddf57a6ae297a908e156
Khoa Pham <khoaphp@yahoo.com>
2016-11-16 05:46:59 -0800

    xong route dang nhap

--- index.js
+++ index.js
@@ -6,11 +6,34 @@ var app = express();
 app.listen(3000);
 app.set('view engine', 'ejs');
 app.set('views', './views');
-var insert = require('./db');
+var insert = require('./db').insert;
+var login = require('./db').login;
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
   res.render('home');
+});
+
+app.get('/dangNhap', function(req, res){
+  res.send( `
+    <form action="/xulydangnhap" method="post">
+      <input type="text" placeholder="username" name="username"/><br><br>
+      <input type="password" placeholder="password" name="password"/><br><br>
+      <input type="submit" value="dang nhap"/>
+    </form>
+  `)
+});
+
+app.post('/xulydangnhap', parser, function(req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+
+  login(username, password, function(){//onFail
+      res.send('Dang nhap that bai');
+  },
+  function(){//onSuccess
+      res.send('Dang nhap thanh cong');
+  })
 });
 
 app.post('/dangky', parser, function(req, res){
